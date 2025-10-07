a2e4bed0a332e26d43822fd90bac6fe4dee50d4d
joroinnoroff <oinojorgen@gmail.com>
2025-10-07 23:31:37 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import recordsRouter from './records.js';
 import eventsRouter from './events.js';
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ["http://localhost:3000", "https://brokken-front-yt8g.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/records', recordsRouter);
